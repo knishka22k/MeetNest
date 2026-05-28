8d157eba4c4c4300a8ef165596083938ad0038cf
Knishka kumari <knishka22k@gmail.com>
2026-05-28 23:37:33 +0530

    CSS improvement of  auth

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import "./LoginSignup.css";
+import "./App.css";
 
 import user_icon from "../Assets/person.png";
 import email_icon from "../Assets/email.png";
