4cde4730cca6c8b4be6496e5aea96e1519148888
Knishka kumari <knishka22k@gmail.com>
2026-05-29 00:13:34 +0530

    Redesigned authentication page

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useState } from "react";
-import "./App.css";
+import "../App.css";
 
-import user_icon from "../Assets/person.png";
-import email_icon from "../Assets/email.png";
-import password_icon from "../Assets/password.png";
+import user_icon from "../assets/person.png";
+import email_icon from "../assets/email.png";
+import password_icon from "../assets/password.png";
 
 import Snackbar from "@mui/material/Snackbar";
 
